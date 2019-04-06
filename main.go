0eecfcc508271d0d71711fcc16ed63c6126ec8ce
Delfer <i@delfer.ru>
2019-04-07 02:21:49 +0300

    schedule implemented

--- main.go
+++ main.go
@@ -1,58 +1,68 @@
 package main
 
 import (
-  "log"
-  "os"
+	"log"
+	"os"
 
-  "github.com/urfave/cli"
+	"github.com/urfave/cli"
 )
 
 func main() {
 
-  app := cli.NewApp()
+	app := cli.NewApp()
 
-  app.Flags = []cli.Flag {
-    cli.StringFlag{
-      Name: "dialect",
-      Value: "postgres",
-      Usage: "mysql, postgres, mssql",
-      EnvVar: "DB_DIALECT",
-    },
-    cli.StringFlag{
-      Name: "db-connection",
-      Value: "host=localhost port=5432 user=admin dbname=gorm password=mypassword sslmode=disable",
-      Usage: "connection parameters",
-      EnvVar: "DB_CONNECTION",
-    },
-    cli.StringFlag{
-      Name: "name",
-      Value: "anonymous",
-      Usage: "hostname to write results",
-      EnvVar: "HOSTNAME",
-    },
-    cli.StringFlag{
-      Name: "paths",
-      Value: "https://commoncrawl.s3.amazonaws.com/crawl-data/CC-MAIN-2019-13/warc.paths.gz",
-      Usage: "url of Common Crawl gzipped paths list",
-      EnvVar: "PATHS_URL",
-    },
-  }
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "dialect",
+			Value:  "postgres",
+			Usage:  "mysql, postgres, mssql",
+			EnvVar: "DB_DIALECT",
+		},
+		cli.StringFlag{
+			Name:   "db-connection",
+			Value:  "host=localhost port=5432 user=admin dbname=gorm password=mypassword sslmode=disable",
+			Usage:  "connection parameters",
+			EnvVar: "DB_CONNECTION",
+		},
+	}
 
-  app.Commands = []cli.Command{
-    {
-      Name:    "schedule",
-      Usage:   "create tasks schedule in DB",
-      Action:  schedule,
-    },
-    {
-      Name:    "parse",
-      Usage:   "parse WARC files",
-      Action:  parse,
-    },
-  }
+	app.Commands = []cli.Command{
+		{
+			Name:   "schedule",
+			Usage:  "schedule tasks in DB",
+			Action: schedule,
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:   "paths-server",
+					Value:  "https://commoncrawl.s3.amazonaws.com/",
+					Usage:  "protocol and server of Common Crawl gzipped paths list",
+					EnvVar: "PATHS_SERVER",
+				},
+				cli.StringFlag{
+					Name:   "paths-uri",
+					Value:  "crawl-data/CC-MAIN-2019-13/warc.paths.gz",
+					Usage:  "uri of Common Crawl gzipped paths list",
+					EnvVar: "PATHS_URI",
+				},
+			},
+		},
+		{
+			Name:   "parse",
+			Usage:  "parse WARC files",
+			Action: parse,
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:   "name",
+					Value:  "anonymous",
+					Usage:  "hostname to write results",
+					EnvVar: "HOSTNAME",
+				},
+			},
+		},
+	}
 
-  err := app.Run(os.Args)
-  if err != nil {
-    log.Fatal(err)
-  }
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- schedule.go
+++ schedule.go
@@ -1,8 +1,13 @@
 package main
 
 import (
-	"net/url"
+	"strconv"
+	"bufio"
+	"compress/gzip"
+	"errors"
 	"fmt"
+	"log"
+	"net/http"
 	"time"
 
 	"github.com/jinzhu/gorm"
@@ -10,19 +15,17 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/urfave/cli"
-	//	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 type Task struct {
-	gorm.Model
-	TaskID     int `gorm:"AUTO_INCREMENT"`
-	Url        *url.URL
+	URL        string `gorm:"PRIMARY_KEY;UNIQUE_INDEX"`
+	WorkerName string
 	Started    *time.Time
 	Completed  *time.Time
-	WorkerName string
 }
 
 func schedule(c *cli.Context) error {
+
 	db, err := gorm.Open(c.GlobalString("dialect"), c.GlobalString("db-connection"))
 	if err != nil {
 		return err
@@ -31,7 +34,59 @@ func schedule(c *cli.Context) error {
 
 	db.AutoMigrate(&Task{})
 
-	fmt.Print("HW!")
+	// Open remote file
+	resp, err := http.Get(c.String("paths-server") + c.String("paths-uri"))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return errors.New(c.String("paths-server") + c.String("paths-uri") + ": " + resp.Status)
+	}
+
+	// Un gzip
+	gzipReader, err := gzip.NewReader(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	log.Println("Got paths, start writing to DB")
+
+	// Read lines from file
+	budReader := bufio.NewReader(gzipReader)
+	// begin a transaction
+	tx := db.Begin()
+	for i := 1; ; i++ {
+		line, isPrefix, err := budReader.ReadLine()
+
+		if err != nil {
+			break
+		}
+
+		if isPrefix {
+			panic("URL in paths list too long")
+		}
+
+		task := Task{URL: c.String("paths-server") + string(line)}
+		tx.Where(&task).FirstOrCreate(&task, &task)
+
+		if i%100 == 0 {
+			fmt.Print(".")
+		}
+
+		if i%1000 == 0 {
+			// split the transaction
+			tx.Commit()
+			tx = db.Begin()
+			fmt.Println()
+			log.Println("Wrote "+ strconv.Itoa(i))
+		}
+	}
+	// commit the transaction
+	tx.Commit()
+
+	log.Println("Done")
 
 	return nil
 }
